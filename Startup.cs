08b4640de6cfc7f648ecd2ed387d556da7911e41
Jim Rogers <thejimrogers@pm.me>
2021-12-10 08:09:54 -0600

    Service Classes - Basic Image Service

--- Startup.cs
+++ Startup.cs
@@ -51,8 +51,7 @@ namespace MoviePro
             services.AddHttpClient();
             services.AddScoped<IRemoteMovieService, TMDBMovieService>();
             services.AddScoped<IDataMappingService, TMDBMappingService>();
-
-
+            services.AddSingleton<IImageService, BasicImageService>()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
