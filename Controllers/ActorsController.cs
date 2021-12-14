9d5a8f05871ac4c5e7713552b1acf3496865bc01
Jim Rogers <thejimrogers@pm.me>
2021-12-14 15:22:39 -0600

    Introductory Feature Set / Actor Detail Page

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -19,7 +19,7 @@ namespace MoviePro.Controllers
 
         private readonly IDataMappingService _mappingService;
 
-        public async Task<IActionResult> Detail(int id)
+        public async Task<IActionResult> Details(int id)
         {
             var actor = await _tmdbMovieService.ActorDetailAsync(id);
             actor = _mappingService.MapActorDetail(actor);
